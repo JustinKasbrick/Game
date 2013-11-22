79f82614fa1e5160690b22dcf391e46a50340b3f
JustinKasbrick <jusam311@hotmail.ca>
2013-11-21 23:28:59 -0600

    Adding savefile screen and stuff.

---
+++ src/com/jkgames/game/views/ContinueScreen.java
@@ -0,0 +1,11 @@
+package com.jkgames.game.views;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brennan
+ * Date: 21/11/13
+ * Time: 10:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ContinueScreen {
+}
