46cae9a93cc6187cd4894bb60f56ad2d298748df
Justin Kasbrick <jusam311@hotmail.ca>
2013-11-25 11:21:18 -0600

    Added new screen for new games.

---
+++ src/com/jkgames/game/views/NewGameScreen.java
@@ -0,0 +1,11 @@
+package com.jkgames.game.views;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Justin.Kasbrick
+ * Date: 11/25/13
+ * Time: 11:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewGameScreen {
+}
